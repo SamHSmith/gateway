74312376111894e39b085a49247d0dedfddcd864
Sam Smith <sam.henning.smith@protonmail.com>
2020-10-29 00:39:50 +0100

    add email

--- src/gateway.c
+++ src/gateway.c
@@ -1,4 +1,5 @@
     Copyright (C) 2020 Sam H Smith
+    Contact: sam.henning.smith@protonmail.com
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
